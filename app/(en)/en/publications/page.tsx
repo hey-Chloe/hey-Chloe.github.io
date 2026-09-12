588ba0c9bdca1d2c96ec760173c9c8753216c118
寇依 <heyxiaoyue@gmail.com>
2026-09-12 16:09:57 +0800

    site: remove English publications page

--- app/(en)/en/publications/page.tsx
+++
@@ -1,3 +0,0 @@
-import { IndexContent,indexMetadata } from "@/components/pages";
-export const metadata=indexMetadata("publications","en");
-export default function Page(){return <IndexContent kind="publications" locale="en"/>;}
